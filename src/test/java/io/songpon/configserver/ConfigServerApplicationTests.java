eb1f8ed07e67cc87bfe61c1f6c13fcf9c975ae29
Songpon Imyen <11411733@LM000C6PVWHYYCT.local>
2024-05-31 10:42:05 +0700

    first commit

---
+++ src/test/java/io/songpon/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.songpon.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
